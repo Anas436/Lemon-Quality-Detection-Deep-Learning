fccb16326a67e92b1d3387a609ce0c6038d2797a
Anas436 <mdanasmondol43@gmail.com>
2023-11-04 03:22:43 +0600

    Deep Learning

---
+++ lemonquality/home/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+def home(request):
+    return render(request, "index.html")
+
+def about(request):
+    return render(request, "about.html")
+
+def contact(request):
+    return render(request, "contact.html")
+
+def login(request):
+    return render(request, "login.html")
+
+def registration(request):
+    return render(request, "registration.html")
+
+
